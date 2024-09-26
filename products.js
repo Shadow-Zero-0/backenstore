0e2d601e0e9d84faf847cf9edec2d1035037bf44
Shadow <qaz889065@gmail.com>
2024-09-27 02:39:25 +0300

    Add files via upload

---
+++ products.js
@@ -0,0 +1,59 @@
+const products = [
+    {
+      id: 1,
+      productName: "Sports shoes",
+      price: 100,
+      imageLink: ["https://sc01.alicdn.com/kf/HTB1Ox8BcQ9E3KVjSZFG76319XXak/Men-s-Shoes-Sports-2019-New-Korean.png",
+       "https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/22241876/2023/9/21/f5bb5c18-6233-47db-8479-be5205a3fea81695294768389WROGNMenColourblockedRunningShoes1.jpg",
+       "https://www.jiomart.com/images/product/original/rvzezcpch2/waan-walking-shoes-for-men-black-white-product-images-rvzezcpch2-4-202212051116.jpg?im=Resize=(500,630)",
+       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6Lm_vsGVSjdFlaz1_rgpg6hBczlyhd-FjfXg9b8fU4r33EKORTpylm1jcFgaH3r4H8So&usqp=CAU",],
+       quantity:1,
+       key:1
+    },
+    {
+      id: 2,
+      productName: "Sports shoes",
+      price: 200,
+      imageLink: ["https://rheb.cc/wp-content/uploads/2019/09/4711-17.jpg",
+       "https://img.ltwebstatic.com/images3_pi/2021/07/22/1626917923b9d93f077fda59862551a404e230a536_thumbnail_720x.jpg",
+       "https://img.ltwebstatic.com/images3_spmp/2024/01/09/f1/1704805198843f474e64fdedb031b1974899166f24_thumbnail_720x.jpg",
+       "https://m.media-amazon.com/images/I/61QyCsgYUWL._AC_SX425_.jpg",],
+       quantity:1,
+       key:2
+    },
+    {
+      id: 3,
+      productName: "Sports shoes",
+      price: 300,
+      imageLink: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSadwS1gFG3cRBf1waQXbZt8dE3s7IvtY-KhxPoirmBWo06vro3UicdBFZ4Uf_TInr-t3s&usqp=CAU",
+       "https://media.karousell.com/media/photos/products/2023/6/7/platform_men_shoes_fashion_clu_1686120383_24ba6400_progressive.jpg",
+       "https://img.ltwebstatic.com/images3_spmp/2023/08/21/c6/1692621860e5e29c286bd4f1462ec1ba844c4f7583_thumbnail_720x.webp",
+       "https://img.ltwebstatic.com/images3_pi/2023/02/14/1676352169395b655a987b7cbf31e4c3624a0c8779_thumbnail_720x.jpg",],
+       quantity:1,
+       key:3
+    },
+    {
+      id: 4,
+      productName: "Sports shoes",
+      price: 400,
+      imageLink: ["https://sc01.alicdn.com/kf/Hf9dd28905c534eb7bcb3f229a009b553h/Wholesale-tn-importar-Air-sneakers-zapatillas-deportivas.jpg",
+       "https://img.ltwebstatic.com/images3_spmp/2024/04/10/71/1712735510dee93f2d4ebbcc3738a77f5e3b1c3ff0_thumbnail_720x.jpg",
+       "https://img.ltwebstatic.com/images3_pi/2022/07/05/16569896201db40a283d139b4349d2691dd3d03b09_thumbnail_720x.webp",
+       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQcOuBAPuuZdoIPaykQKlTi0nAKlYLxdLXJn8YTSaQBWRGMHjPmi-_fR-XYu5wCN-DKXdI&usqp=CAU",],
+       quantity:1,
+       key:4
+    },    {
+      id: 5,
+      productName: "Sports shoes",
+      price: 500,
+      imageLink: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGGygu79ISILRUq28RKhGnSEBTv3Pjgis89w&s",
+       "https://ride.shimano.com/cdn/shop/articles/How-should-your-bike-shoes-fit.jpg?v=1701884630",
+       "https://media.istockphoto.com/id/531912454/photo/female-hands-lacing-running-shoes-closeup.jpg?s=612x612&w=0&k=20&c=U47vJHb8I7JW2lMpad7hwhE4OmITpLdHBAGAO2j2HwE=",
+       "https://baccabucci.com/cdn/shop/products/MG_5242.jpg?v=1633514122",],
+       quantity:1,
+       key:5
+
+    },
+    
+   ];
+   module.exports = products;
